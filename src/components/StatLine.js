c672177ae579f85ff38cfead324cfc627c612911
commanderdestro <garrett.hester+destro@gmail.com>
2022-05-06 09:15:22 -0500

    dbls

--- src/components/StatLine.js
+++ src/components/StatLine.js
@@ -1,11 +1,20 @@
 import './StatLine.css';
 
-const StatLine = ({ roll, countRolls, rolls }) => {
+const StatLine = ({ roll = 'doubles', countRolls, rolls }) => {
   return (
     <>
       <div className='stat-line'>
-        <div className='die'>{roll[0]}</div>
-        <div className='die'>{roll[1]}</div>
+        {roll === 'doubles' ? (
+          <>
+            <div classname='die'>Dbl</div>
+            <div classname='die'>Dbl</div>
+          </>
+        ) : (
+          <>
+            <div className='die'>{roll[0]}</div>
+            <div className='die'>{roll[1]}</div>
+          </>
+        )}
         <div className='count'>{`${countRolls(rolls, roll)}`}</div>
         <div className='percent'>{`${
           countRolls(rolls, roll) === 0
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,16 +4,21 @@ import StatLine from './StatLine';
 const Stats = ({ playerName }) => {
   const rolls = Lockr.get(playerName);
 
-  const countRolls = (rolls, roll) => {
+  const countRolls = (rolls, roll = 'doubles') => {
     let count = 0;
-    for (let i = 0; i < rolls.length; i++) {
-      if (rolls[i][0] === roll[0] && rolls[i][1] === roll[1]) {
-        count += 1;
-      } else if (rolls[i][0] === roll[1] && rolls[i][1] === roll[0]) {
-        count += 1;
+    if (rolls && roll === 'doubles') {
+      for (let i = 0; i < rolls.length; i++) {
+        if (rolls[i][0] === rolls[i][1]) count += 1;
+      }
+    } else if (rolls && roll !== 'doubles') {
+      for (let i = 0; i < rolls.length; i++) {
+        if (rolls[i][0] === roll[0] && rolls[i][1] === roll[1]) {
+          count += 1;
+        } else if (rolls[i][0] === roll[1] && rolls[i][1] === roll[0]) {
+          count += 1;
+        }
       }
     }
-
     return count;
   };
 
@@ -25,27 +30,28 @@ const Stats = ({ playerName }) => {
   return (
     <>
       <div>{playerName}'s Roll History</div>
-        <StatLine roll={['1','1']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['1','2']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['1','3']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['1','4']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['1','5']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['1','6']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['2','2']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['2','3']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['2','4']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['2','5']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['2','6']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['3','3']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['3','4']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['3','5']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['3','6']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['4','4']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['4','5']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['4','6']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['5','5']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['5','6']} rolls={rolls} countRolls={countRolls} />
-        <StatLine roll={['6','6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '1']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '2']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '3']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '4']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '5']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['1', '6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['2', '2']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['2', '3']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['2', '4']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['2', '5']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['2', '6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['3', '3']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['3', '4']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['3', '5']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['3', '6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['4', '4']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['4', '5']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['4', '6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['5', '5']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['5', '6']} rolls={rolls} countRolls={countRolls} />
+      <StatLine roll={['6', '6']} rolls={rolls} countRolls={countRolls} />
       <div></div>
       <button onClick={handleClear}>Clear History</button>
     </>
