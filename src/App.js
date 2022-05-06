2d6037be20ff217f21a49ce70715934dcf071786
commanderdestro <garrett.hester+destro@gmail.com>
2022-05-06 12:26:49 -0500

    remove wager

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
 
-import { EditText } from 'react-edit-text';
-import 'react-edit-text/dist/index.css';
-
 import Player from './components/Player';
 
 function App() {
   return (
     <div className='container'>
-      <EditText id='wager-box' className='wager' placeholder='Wager' />
       <div className='player-container'>
         <Player className='player' placeholder='Player1' />
         <Player className='player' placeholder='Player2' />
